3d1bfd60edde385c3a3a31ce83f8f1fe7fb72b34
lsaOntact <seula.lee@ontacthealth.com>
2025-12-05 09:54:25 +0900

    feat: 카드뉴스 상세+수정 기능 추가

--- src/component/contentManage/modal/CardEditModal.jsx
+++ src/component/contentManage/modal/CardEditModal.jsx
@@ -8,8 +8,9 @@ import {
   Image,
   Space,
   Upload,
+  message,
 } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
+import { UploadOutlined, InboxOutlined } from "@ant-design/icons";
 import { visibleOptions, selectStyle, categoryOptions } from "../settings";
 import { useState } from "react";
 
@@ -26,14 +27,28 @@ const CardEditModal = ({ open, onCancel, data }) => {
   const [title, setTitle] = useState(data.title);
   const [thumbnailPreview, setThumbnailPreview] = useState(null);
   const [coverPreview, setCoverPreview] = useState(null);
-  const labelList = [
-    { title: "공개여부", required: true },
-    { title: "카테고리", required: true },
-    { title: "제목", required: true },
-    { title: "썸네일", required: true },
-    { title: "커버 이미지", required: true },
-    { title: "카드 뉴스 본문", required: true },
-  ];
+  const [cardNewsFiles, setCardNewsFiles] = useState([]);
+
+  const handleSave = async () => {
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("visible", visible);
+    formData.append("category", category);
+
+    cardNewsFiles.forEach((file) => {
+      if (file.originFileObj) {
+        formData.append("cardNewsFiles", file.originFileObj);
+      }
+    });
+
+    try {
+      const response = await fetch("/api/card-news/update", {
+        method: "POST",
+        body: formData,
+      });
+    } catch (error) {}
+  };
 
   return (
     <Modal
@@ -44,6 +59,7 @@ const CardEditModal = ({ open, onCancel, data }) => {
       }
       open={open}
       onCancel={onCancel}
+      styles={{ body: { maxHeight: "70vh", overflowY: "auto" } }}
       footer={
         <Flex justify="space-between">
           <Button danger onClick={() => console.log("삭제")}>
@@ -171,10 +187,71 @@ const CardEditModal = ({ open, onCancel, data }) => {
           </Space>
 
           <strong>카드 뉴스 본문</strong>
-          <div>{data.likes}</div>
+          <Space direction="vertical" style={{ width: "100%" }}>
+            <Typography.Text style={{ color: "#7d7d7d" }}>
+              썸네일과 커버 이미지를 제외한 카드뉴스 본문만 업로드 해주세요.
+            </Typography.Text>
+
+            <Dragger
+              cardNewsFiles={cardNewsFiles}
+              setCardNewsFiles={setCardNewsFiles}
+            />
+          </Space>
         </div>
       </>
     </Modal>
+  );
+};
+
+const Dragger = ({ cardNewsFiles, setCardNewsFiles }) => {
+  const props = {
+    name: "multiple file",
+    multiple: true,
+    maxCount: 10,
+    listType: "picture-card",
+    accept: "image/*",
+    showUploadList: {
+      showPreviewIcon: false,
+    },
+    // 업로드한 파일을 서버로 전송할 API 엔드포인트 URL
+    // action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
+    onChange(info) {
+      const { status } = info.file;
+      setCardNewsFiles(info.fileList);
+      // if (status !== "uploading") {
+      //   console.log(info.file, info.fileList);
+      // }
+      // if (status === "done") {
+      //   message.success(`${info.file.name} file uploaded successfully.`);
+      // } else if (status === "error") {
+      //   message.error(`${info.file.name} file upload failed.`);
+      // }
+    },
+    beforeUpload(file) {
+      const isLt5M = file.size / 1024 / 1024 < 5;
+      if (!isLt5M) {
+        message.error(`${file.name}의 크기가 5mb를 초과합니다.`);
+        return Upload.LIST_IGNORE;
+      }
+      // return isLt5M || Upload.LIST_IGNORE;
+      return false; // 자동 업로드 방지
+    },
+    onDrop(e) {
+      console.log("Dropped files", e.dataTransfer.files);
+    },
+  };
+  return (
+    <Upload.Dragger {...props} style={{ height: 100, marginBottom: 10 }}>
+      <p className="ant-upload-drag-icon" style={{ marginBottom: 5 }}>
+        <InboxOutlined />
+      </p>
+      <p className="ant-upload-text">
+        이미지를 끌어오거나 파일을 선택해주세요.
+      </p>
+      <p className="ant-upload-hint" style={{ marginTop: 0 }}>
+        (최대 10장, 장당 5Mb까지 업로드 가능합니다.)
+      </p>
+    </Upload.Dragger>
   );
 };
 
